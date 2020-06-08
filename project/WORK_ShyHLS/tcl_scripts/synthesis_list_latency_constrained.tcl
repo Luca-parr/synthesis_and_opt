c50db6ff4f9eeb119f87a45001aa171b75fdfbe1
Luca-parr <tartuchino@hotmail.it>
2020-06-08 20:53:03 +0200

    new commit

--- project/WORK_ShyHLS/tcl_scripts/synthesis_list_latency_constrained.tcl
+++ project/WORK_ShyHLS/tcl_scripts/synthesis_list_latency_constrained.tcl
@@ -10,7 +10,7 @@ read_library ./data/RTL_libraries/RTL_lib_1.txt
 #puts "$node_att"
 #puts "$edge_att"
 #puts "$lib_fu_att"
-
+set dfg_late {}
 set latency_schedule [list_latency 18]
 foreach pair $latency_schedule {
 set node_id [lindex $pair 0]
@@ -18,10 +18,10 @@ set node_op [ get_attribute $node_id operation ]
 set node_delay [ lindex $pair 2 ]
 set start_time [lindex $pair 1]
 puts "node $node_id doing $node_op with delay $node_delay starts @$start_time"
-
+lappend dfg_late "$node_id $start_time"
 }
 print_dfg ./data/out/fir.dot
-print_scheduled_dfg $latency_schedule ./data/out/fir_latency.dot
+print_scheduled_dfg $dfg_late ./data/out/fir_latency.dot
 
 
 
